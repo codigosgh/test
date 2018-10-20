b03810ebe36602ef3f3670696fe073e0914ae291
Ariel Larraguibel S <38830483+codigosgh@users.noreply.github.com>
2018-10-20 04:54:21 -0400

    Subir

---
+++ testAoi/testAoi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testAoi.Startup))]
+namespace testAoi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
